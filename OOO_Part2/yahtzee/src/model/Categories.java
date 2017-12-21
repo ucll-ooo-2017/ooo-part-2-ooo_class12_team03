480d345403b9d17b39b98b4d4d6c6310c22480d2
Jordy Rymenants <jordy.rymenants@outlook.com>
2017-12-21 21:16:50 +0100

    Story 5

---
+++ OOO_Part2/yahtzee/src/model/Categories.java
@@ -0,0 +1,17 @@
+package model;
+
+public enum Categories {
+	ACES,
+	TWOS,
+	THREES,
+	FOURS,
+	FIVES,
+	SIXES,
+	THREE_OF_A_KIND,
+	FOUR_OF_A_KIND,
+	FULL_HOUSE,
+	SMALL_STRAIGHT,
+	LARGE_STRAIGHT,
+	YAHTZEE,
+	CHANCE
+}
--- OOO_Part2/yahtzee/src/view/DicePane.java
+++ OOO_Part2/yahtzee/src/view/DicePane.java
@@ -7,7 +7,11 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import model.Categories;
 
 import java.util.Observable;
 import java.util.Observer;
@@ -17,6 +21,7 @@ public class DicePane extends GridPane implements Observer {
 	private PlayerController playerController;
 
 	private Button rollButton;
+	private ComboBox<Categories> categoriesComboBox;
 
 	public DicePane(PlayerController playerController) {
 		this.playerController = playerController;
@@ -28,18 +33,33 @@ public class DicePane extends GridPane implements Observer {
 		rollButton = new Button("Roll Dice");
 		rollButton.setOnAction(new ClickHandler());
 		add(rollButton, 0, 1);
+		GridPane.setVgrow(rollButton, Priority.ALWAYS);
 
+		HBox diceBox = new HBox();
+		diceBox.setSpacing(10);
 		int pos = 0;
 		for (DieController dieController : playerController.getDieControllers()) {
-			add(dieController.getView(), pos, 2);
+			diceBox.getChildren().add(dieController.getView());
 			pos++;
 		}
+		add(diceBox, 0, 2);
+		GridPane.setVgrow(diceBox, Priority.ALWAYS);
 
+		HBox asideDiceBox = new HBox();
+		asideDiceBox.setSpacing(10);
 		pos = 0;
 		for (DieAsideController dieAsideController : playerController.getDieAsideControllers()) {
-			add(dieAsideController.getView(), pos, 0);
+			asideDiceBox.getChildren().add(dieAsideController.getView());
 			pos++;
 		}
+		add(asideDiceBox, 0, 0);
+		GridPane.setVgrow(asideDiceBox, Priority.ALWAYS);
+
+		categoriesComboBox = new ComboBox<>();
+		categoriesComboBox.getItems().addAll(Categories.values());
+		categoriesComboBox.setValue(categoriesComboBox.getItems().get(0));
+		add(categoriesComboBox, 0, 3);
+		GridPane.setVgrow(categoriesComboBox, Priority.ALWAYS);
 	}
 
 	@Override
